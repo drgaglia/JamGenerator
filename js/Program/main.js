661b1eb5ece20cce1d16a08cb7963158b71e57ba
dgagliardi <dgagliardi@unifrax.com>
2019-05-01 16:37:10 -0400

    Can now generate random chords fo a given key and very crudely display them on the screen. Should probably just continue working in the console so index.html doesn't become an ungodly mess of feature testing.

--- js/Program/main.js
+++ js/Program/main.js
@@ -1,3 +1,7 @@
+let isPlaying = false;
+let tempo = 2000;
+let key;
+let playbackTimer;
 
 window.onload = function () {
     init ();
@@ -6,4 +10,33 @@ window.onload = function () {
 function init () {
     modeGroups = generateInitialModes ( modeGroups );
     initializeUI ();
+    key = new Key ( CHROMATIC_SCALE [ 0 ].enharmonics [ 2 ], modeGroups [ 0 ][ 0 ] );
+}
+
+/*\
+|*|||||||||||||||||||||||||||||||||||||||||||||||
+| ***************** Dev Below ***************** |
+|*|||||||||||||||||||||||||||||||||||||||||||||||
+\*/
+
+function getRandomKeyChord () {
+    scaleDegree = Math.floor ( Math.random () * key.modeSteps.length );
+    let chord = "";
+    key.keyChords [ scaleDegree ].forEach ( function ( note ) {
+        chord += note.pitches[ 0 ] + " ";
+    } ); 
+    document.getElementById ( "chord-display" ).innerHTML = chord;
+}
+
+function togglePlayback ( isPlaying ) {
+    if ( isPlaying ) {
+        playbackTimer = setInterval ( getRandomKeyChord, tempo );
+    } else {
+        clearInterval ( playbackTimer );
+    }   
+}
+
+document.getElementById ( "button-play" ).onclick = function () {
+    isPlaying = !isPlaying;
+    togglePlayback ( isPlaying );
 }
